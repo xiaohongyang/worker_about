363bc81837048c0e0ebbc2fe58dfd700c84118d7
xiaohongyang <xiaohongyang@adsage.com>
2017-07-13 18:17:34 +0800

    #

---
+++ zhanwenchuang/www/art_index/js/down.js
@@ -0,0 +1,7 @@
+//判断访问终端
+var ios ='https://itunes.apple.com/cn/app/id1183642892?mt=8';
+var android = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.wenchuangbj.android';
+
+
+document.getElementById("android").href = android;
+document.getElementById("ios").href = ios;
